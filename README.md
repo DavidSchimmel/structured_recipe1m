5b233de57fdc4f6db1136d71577066ea334a6bd0
DavidSchimmel <david.schimmel@gmx.de>
2024-02-27 01:54:43 +0100

    add docs

--- README.md
+++ README.md
@@ -1,11 +1,45 @@
 # Generate different graphs for Recipe1MSubs
 
-This library creates several graphical representations for recipes in the recipe1MSubs dataset. To use this, you need to obtain the recipe1MSubs  dataset.
+This library parses recipes from Recipe1MSubs and generates various graph representations from them.
 
-- code parts taken or adapted from https://github.com/boschresearch/EaT-PIM/blob/main/eatpim/etl can be found in the recipe parser and are marked in the function level comments
+## Prerequisits
 
-- `RECIPES_EXTENDED_PATH` can point to a file with the default recipe1MSubs recipe data, contained objects to only include `id`, `ingredients`, and `instructions`
+### Inputs
+
+- from Recipe1MSubs (follow the data preparation step [here](https://github.com/DavidSchimmel/gismo/blob/um_mt_extensions/gismo/README.md#data-preparation) if you need those files):
+  - `train_comments_subs.pkl`
+  - `test_comments_subs.pkl`
+  - `val_comments_subs.pkl`
+- from ArcSubs:
+  - `tmp_total_arcelik_only_comments.pkl`
+    - this is needed if you want the reduced flow-graph like recipe representations that are filtered such that only recipes that are part of the given samples are represented.
+    - this file must be a set of dictionaries, where each dictionary must contain an `["id"]` key, where the values should be recipes from Recipe1MSubs
+    - if the recipe IDs don't match, the filter will filter out all recipes and no graph will be represented.)
+- other:
+  - `extended_recipes_with_quantities.json`
+    -
+
+In the process, several intermediary files are created in the `intermediary` directory. They are used to quickly resume processing from those intermediary results and can be removed if no longer needed.
+
+### Dependencies
+
+- The conda environment can be loaded from the accompanying [environment.yml](./environment.yml)
+- You may need to install spacy's `en-core-web-trf` separately
+
+## Setup
+
+- import the required files or set the paths in the main.py, set up the conda environment
+- set up the config file, selecting the graphs you want to generate
+
+## Usage
+
+- simply run the `main()` function of the `main.py` file
+- depending on the setttings in the config.yaml, different graphs will be generated
+
+## Outputs
 
 
-Dependencies:
-- spacy with `en_core_web_trf` model pretrained
+
+## Credits
+
+Code parts taken or adapted from https://github.com/boschresearch/EaT-PIM/blob/main/eatpim/etl can be found in the recipe parser and are marked in the function level comments
--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import os
 import spacy
 import pickle
+import yaml
 
 from data import loader
 from structure_extraction import (graph_generator, recipe_parser)
 
-GENERATE_RECIPE_CONTEXT_SIMPLE_GRAPH = False
-GENERATE_RECIPE_CONTEXT_WITH_SIMPLE_ACTIONS = False
-GENERATE_RECIPE_CONTEXT_WITH_INTERMEDIARY_ACTIONS = False
-GENERATE_RECIPE_CONtEXT_WITH_INTERMEDIARY_ACTIONS_ARC_RECS_ONLY = True
-
+config = yaml.safe_load(open(os.path.abspath("./config.yaml")))
 
 RECIPES_EXTENDED_PATH = os.path.abspath("./data/input/extended_recipes_with_quantities.json")
 TRAIN_COMMENTS_PATH = os.path.abspath("./data/input/train_comments_subs.pkl")
 TEST_COMMENTS_PATH = os.path.abspath("./data/input/test_comments_subs.pkl")
 VAL_COMMENTS_PATH = os.path.abspath("./data/input/val_comments_subs.pkl")
-MINIMAL_RECIPES_PATH = os.path.abspath("./data/input/minimal_recipe_dataset.pkl") # this one will be the one that we calculate with. It should look like a dict with recipe_id as key and value as dict with keys "ingredients" and "instructions" and the corresponding arrays
 ARC_ONLY_SAMPLES_PATH = os.path.abspath("./data/input/tmp_total_arcelik_only_comments.pkl") # reduced recipe set that only contains recipes in the arc only dataset
+
+MINIMAL_RECIPES_PATH = os.path.abspath("./data/input/minimal_recipe_dataset.pkl") # this one will be the one that we calculate with. It should look like a dict with recipe_id as key and value as dict with keys "ingredients" and "instructions" and the corresponding arrays
 
 SIMPLE_RECIPE_INGREDIENT_GRAPH_DIR = os.path.abspath("./data/output/simple_recipe_ingredient_graph/")
 SIMPLE_RECIPE_CONTEXT_SIMPLE_INSTRUCTIONS_DIR = os.path.abspath("./data/output/simple_recipe_with_context_simple_instructions/")
@@ -65,7 +63,7 @@ def main():
             pickle.dump(ingredient_variant_to_node_index, file)
 
 
-    if GENERATE_RECIPE_CONTEXT_SIMPLE_GRAPH:
+    if config["target_graph"]["bipartite_ingredients"]:
         graph_generator.generateRecipeIngredientGraph(recipes, ingredient_variant_to_node_index, raw_nodes, raw_edges, SIMPLE_RECIPE_INGREDIENT_GRAPH_DIR)
 
     recipes_with_ingredients_to_steps = None
@@ -77,13 +75,13 @@ def main():
         with open(REC_W_INGR_CNT_TO_STEP_PATH, "wb") as rec_with_ingr_cnt_to_step_file:
             pickle.dump(recipes_with_ingredients_to_steps, rec_with_ingr_cnt_to_step_file)
 
-    if GENERATE_RECIPE_CONTEXT_WITH_SIMPLE_ACTIONS:
+    if config['target_graphs']['bipartite_ingredients']:
         graph_generator.generateRecipeInstructionsGraph(recipes, parsed_recipes, recipes_with_ingredients_to_steps, distinct_verbs, ingredient_variant_to_node_index, raw_nodes, raw_edges, SIMPLE_RECIPE_CONTEXT_SIMPLE_INSTRUCTIONS_DIR)
 
-    if GENERATE_RECIPE_CONTEXT_WITH_INTERMEDIARY_ACTIONS:
+    if config["target_graph"]["flow"]:
         graph_generator.generateStructuredRecipeInstructionsGraph(recipes, parsed_recipes, recipes_with_ingredients_to_steps, distinct_verbs, ingredient_variant_to_node_index, raw_nodes, raw_edges, SIMPLE_RECIPE_CONTEXT_STRUCTURED_INSTRUCTIONS_DIR)
 
-    if SIMPLE_RECIPE_CONTEXT_STRUCTURED_INSTRUCTIONS_ARC_DIR:
+    if config["target_graph"]["flow_reduced"] and SIMPLE_RECIPE_CONTEXT_STRUCTURED_INSTRUCTIONS_ARC_DIR:
         # because encoding all recipes with intermediary nodes leads to very many nodes, try if the model can be reasonably evaluated by encoding only the arc recipes (recipes that are contained in the arc_subs ground truth substitution data set)
         with open(ARC_ONLY_SAMPLES_PATH, "rb") as arc_only_file:
             arc_only_samples = pickle.load(arc_only_file)
